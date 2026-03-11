2821ae4790468409b7804bf4f8cbb8b880e5762c
cksdn1223 <rlacksdn1223@naver.com>
2026-03-11 18:56:41 +0900

    feat: 야구 객체 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
 public class Application {
+
+    public static class BaseBall {
+        private int[] strikeNumber;
+
+        private boolean checkNumber(String number) {
+            return true;
+        }
+    }
+
+
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+
     }
 }
