3c336422145ac98f877e622b6ff4edd96eef4e8b
cksdn1223 <rlacksdn1223@naver.com>
2026-03-11 20:33:35 +0900

    feat: 야구 객체 생성시 숫자 3개 할당

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
 
     public static class BaseBall {
-        private int[] strikeNumber;
+        private int[] strikeNumber = new int[3];
 
-        private boolean checkNumber(String number) {
+        private static boolean checkNumber(String number) {
             return true;
+        }
+
+        // 생성시 1~9 랜덤 생성해서 집어넣음
+        public BaseBall() {
+            this.strikeNumber[0] = Randoms.pickNumberInRange(1, 9);
+            this.strikeNumber[1] = Randoms.pickNumberInRange(1, 9);
+            this.strikeNumber[2] = Randoms.pickNumberInRange(1, 9);
         }
     }
 
 
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-
+        BaseBall baseBall = new BaseBall();
     }
 }
